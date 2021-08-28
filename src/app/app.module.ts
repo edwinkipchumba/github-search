695ff7876ff95c3753b4d71d847479c40caaa5d5
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-28 09:49:49 +0300

    adding custom directive to underline username on hove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HoverDirective } from './directive/hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HoverDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directive/hover.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appHover]'
+})
+export class HoverDirective {
+
+  constructor(private elem:ElementRef) { }
+
+  @HostListener("mouseenter") onMouseEnter(){
+    this.textDeco("underLine")
+  }
+
+  @HostListener("mouseleave") onMouseLeave(){
+    this.textDeco("none")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+  }
+}
